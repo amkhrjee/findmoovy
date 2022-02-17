5a3ba1474834ff7bce095e0da3cf465f9603023f
Aniruddh Mukherjee <aniruddha.xccentric@gmail.com>
2022-02-17 16:49:57 +0530

    added movie-card component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { RouterLink } from "vue-router";
 import { debounce } from "lodash-es";
+import MovieCard from "./MovieCard.vue";
 
 const searchText = ref("");
 const movieList = ref([]);
@@ -68,17 +69,7 @@ fetch(`https://www.omdbapi.com/?i=tt5834426&apikey=9e23561e`)
         </RouterLink>
       </div>
     </form>
-    <div class="movie-list">
-      <ul>
-        <li v-for="movie in movieList" :key="movie.imdbID">
-          <RouterLink :to="'/movies/' + movie.imdbID">
-            <img class="poster" :src="movie.Poster" :alt="movie.Title + ' Poster'" />
-          </RouterLink>
-          <div class="type">{{ movie.Type }}</div>
-          <h3>{{ movie.Title }}</h3>
-        </li>
-      </ul>
-    </div>
+    <MovieCard :movie-list="movieList" />
     <div class="featured-card">
       <div class="featured-text">🌟 Featured</div>
       <RouterLink to="/movies/tt11198330">
@@ -165,50 +156,6 @@ button {
 }
 .submit:focus {
   background-color: #3c3c3b;
-}
-ul {
-  padding: 1rem;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 1rem;
-}
-li {
-  background: #3c3c3b;
-  position: relative;
-
-  border-radius: 12px;
-
-  height: auto;
-  overflow: hidden;
-  color: #fff;
-}
-.poster {
-  height: 18rem;
-  display: block;
-  object-fit: cover;
-  object-position: center;
-}
-li h3 {
-  padding: 1rem;
-  color: #ebebd3;
-}
-
-li h4 {
-  padding: 1rem;
-  color: #ebebd3;
-}
-
-.type {
-  position: absolute;
-  left: 0;
-  top: 1rem;
-  color: #fff;
-  background-color: rgba(0, 0, 0, 0.6);
-  text-transform: capitalize;
-  border-radius: 0 10px 10px 0;
-  padding: 0.5rem;
-  font-weight: 600;
-  backdrop-filter: blur(5px);
 }
 
 .featured-text {
