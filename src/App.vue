f171742f7fdc6a97bbb98093148c4967d3da2321
Aniruddh Mukherjee <aniruddha.xccentric@gmail.com>
2022-02-18 15:05:03 +0530

    added repo link

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import { RouterLink, RouterView } from "vue-router";
     <RouterLink to="/">
       <h2>FindMoovy</h2>
     </RouterLink>
+    <RouterLink to="https://github.com/amkhrjee/findmoovy">
+      <img class="gh-logo" src="./assets/ghlogo.png" alt="github logo" />
+    </RouterLink>
   </header>
   <main>
     <RouterView />
@@ -14,7 +17,7 @@ import { RouterLink, RouterView } from "vue-router";
   <footer>
     <span>
       Made with
-      <span style="color: #42b883; margin: 0; padding: 0;">Vue3</span> & 🍵 by
+      <span style="color: #42b883; margin: 0; padding: 0">Vue3</span> & 🍵 by
       <a href="https://www.twitter.com/amkhrjee" target="_blank">amkhrjee</a>
     </span>
   </footer>
@@ -32,16 +35,22 @@ import { RouterLink, RouterView } from "vue-router";
 body {
   background-color: #000000;
 }
+
 header {
   display: flex;
   font-weight: 600;
   background-color: #0a1128;
   height: 4em;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+}
+.gh-logo {
+  width: 1.5rem;
+  margin-right: 1rem;
 }
 header h2 {
   color: #da4167;
+  margin-left: 1rem;
 }
 a {
   text-decoration: none;
