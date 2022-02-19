64e04cc8352d8dc64991e47cf830af246b50c4b3
Aniruddh Mukherjee <aniruddha.xccentric@gmail.com>
2022-02-19 18:37:55 +0530

    updated: app with tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
+  <header class="flex justify-between items-center h-16 bg-blue-900 font-bold">
     <RouterLink to="/">
-      <h2>FindMoovy</h2>
+      <h2 class="text-red-400 text-lg ml-4">FindMoovy</h2>
     </RouterLink>
     <a
       href="https://github.com/amkhrjee/findmoovy"
@@ -13,17 +13,24 @@ import { RouterLink, RouterView } from "vue-router";
       target="_blank"
       rel="noopener noreferrer"
     >
-      <img class="gh-logo" src="./assets/ghlogo.png" alt="github logo" />
+      <img class="w-6 mr-4" src="./assets/ghlogo.png" alt="github logo" />
     </a>
   </header>
-  <div class="wrapper">
-    <main>
+  <div class="h-screen flex justify-between flex-col">
+    <main class="h-auto">
       <RouterView />
     </main>
-    <footer>
-      <span>
+    <footer class="relative bottom-0 text-slate-400 flex justify-center">
+      <span class="text-sm p-4">
         Made with
-        <span style="color: #42b883; margin: 0; padding: 0">Vue3</span> & 🍵 by
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          style="color: #42b883; margin: 0; padding: 0"
+          >Vue3</a
+        >
+        & 🍵 by
         <a href="https://www.twitter.com/amkhrjee" target="_blank">amkhrjee</a>
       </span>
     </footer>
@@ -44,46 +51,6 @@ body {
   font-size: 13px;
 }
 
-header {
-  display: flex;
-  font-weight: 600;
-  background-color: #0a1128;
-  height: 4em;
-  justify-content: space-between;
-  align-items: center;
-}
-.gh-logo {
-  width: 1.5rem;
-  margin-right: 1rem;
-}
-header h2 {
-  color: #da4167;
-  margin-left: 1rem;
-}
-a {
-  text-decoration: none;
-}
-
-main {
-  height: auto;
-}
-.wrapper {
-  height: 90vh;
-  display: flex;
-  flex-flow: column;
-  justify-content: space-between;
-}
-footer {
-  position: relative;
-  bottom: 0;
-  color: #758283;
-  display: flex;
-  justify-content: center;
-}
-footer span {
-  font-size: 0.8rem;
-  padding: 1rem;
-}
 footer a {
   text-decoration: none;
   color: #1da1f2;
