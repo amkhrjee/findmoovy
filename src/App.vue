81a8bcc758fca3becef87968ac116fdc5b1bff54
Aniruddh Mukherjee <aniruddha.xccentric@gmail.com>
2022-03-02 19:30:20 +0530

    refactored footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
-import { NSwitch, NButton, NConfigProvider, NH1, NSpace, NH2, NH3 } from "naive-ui";
-import { Sun, Moon, Bookmark, Github, Searchengin } from "@vicons/fa";
+import { NSwitch, NButton, NConfigProvider, NSpace, NP, NText, NA } from "naive-ui";
+import { Sun, Moon, Bookmark, Github, ExternalLinkAlt } from "@vicons/fa";
 import { Icon } from "@vicons/utils";
 
 const darkMode = ref(true);
@@ -25,62 +25,70 @@ const railStyle = ({ focused, checked }) => {
 </script>
 
 <template>
-  <n-config-provider>
-    <n-space class="h-16 bg-navbar" justify="space-between" align="center">
-      <RouterLink to="/">
-        <div class="text-title ml-4 text-xl font-bold">🎬 FindMoovy</div>
-      </RouterLink>
-      <n-space justify="space-around" align="center">
-        <n-switch v-model:value="darkMode" size="large" :rail-style="railStyle">
-          <template #checked-icon>
-            <Icon color="#808080">
-              <Moon />
-            </Icon>
-          </template>
-          <template #unchecked-icon>
-            <Icon color="#da4163">
-              <Sun />
-            </Icon>
-          </template>
-        </n-switch>
-        <RouterLink to="/watchlist">
-          <n-button secondary type="error">
-            <Icon>
-              <Bookmark />
-            </Icon>
-          </n-button>
-        </RouterLink>
-        <n-button class secondary type="info">
-          <Icon size="1.2rem" color="white">
-            <Github />
+  <n-space class="h-16 bg-navbar" justify="space-between" align="center">
+    <RouterLink to="/">
+      <div class="text-title ml-4 text-xl font-bold">🎬 FindMoovy</div>
+    </RouterLink>
+    <n-space justify="space-around" align="center">
+      <n-switch v-model:value="darkMode" size="large" :rail-style="railStyle">
+        <template #checked-icon>
+          <Icon color="#808080">
+            <Moon />
+          </Icon>
+        </template>
+        <template #unchecked-icon>
+          <Icon color="#da4163">
+            <Sun />
+          </Icon>
+        </template>
+      </n-switch>
+      <RouterLink to="/watchlist">
+        <n-button secondary type="error">
+          <Icon>
+            <Bookmark />
           </Icon>
         </n-button>
-      </n-space>
+      </RouterLink>
+      <n-button class secondary type="info">
+        <Icon size="1.2rem" color="white">
+          <Github />
+        </Icon>
+      </n-button>
     </n-space>
-    <!-- <n-space justify="center">
+  </n-space>
+  <!-- <n-space justify="center">
       <Icon size>
         <Searchengin />
       </Icon>
-    </n-space>-->
-    <div class="h-screen flex justify-between flex-col">
-      <main class="h-auto">
-        <RouterView />
-      </main>
-      <footer class="relative bottom-0 text-grey flex justify-center dark:bg-black">
-        <span class="text-sm p-4">
-          Made with
-          <a
-            href="https://vuejs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            style="color: #42b883; margin: 0; padding: 0"
-          >Vue3</a>
-          & 🍵 by
-          <a href="https://www.twitter.com/amkhrjee" target="_blank">amkhrjee</a>
-        </span>
-      </footer>
-    </div>
-  </n-config-provider>
+  </n-space>-->
+  <div class="h-screen flex justify-between flex-col">
+    <main class="h-auto">
+      <RouterView />
+    </main>
+    <n-space justify="center">
+      <n-p depth="3">
+        Made with
+        <a href="https://vuejs.org" target="_blank" rel="noopener noreferrer">
+          <n-text type="success">
+            Vue3
+            <Icon size="0.6rem">
+              <ExternalLinkAlt />
+            </Icon>
+          </n-text>
+        </a>
+
+        <n-text class="ml-1" depth="3">and 🍵 by</n-text>
+        <a href="https://www.twitter.com/amkhrjee" target="_blank" rel="noopener noreferrer">
+          <n-text depth="3" type="info">
+            amkrjee
+            <Icon size="0.6rem">
+              <ExternalLinkAlt />
+            </Icon>
+          </n-text>
+        </a>
+      </n-p>
+    </n-space>
+  </div>
 </template>
 
 <style>
