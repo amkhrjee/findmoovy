b40c1591e0a808cec79ebaa1f43a4cd8b4a9918b
Aniruddh Mukherjee <aniruddha.xccentric@gmail.com>
2022-03-09 17:10:44 +0530

    added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,11 @@ const router = createRouter({
       component: () =>
         import(/*webpackChunkName: "movie"*/ "../views/Results.vue"),
     },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "404",
+      component: () => import(/*webpackChunkName: "404"*/ "../views/404.vue"),
+    },
   ],
 });
 
---
+++ src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <n-result
+    status="404"
+    title="404 Not Found"
+    description="The page you are looking for doesn't exist."
+  >
+    <template #footer>
+      <n-button @click="router.push('/')">
+        Go back to Home
+        <template #icon>
+          <Icon>
+            <ArrowCircleRight />
+          </Icon>
+        </template>
+      </n-button>
+    </template>
+  </n-result>
+</template>
+<script setup>
+import { Icon } from "@vicons/utils";
+import { ArrowCircleRight } from "@vicons/fa";
+import router from "../router";
+</script>
